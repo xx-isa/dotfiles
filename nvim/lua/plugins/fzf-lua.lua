4dc85e4bfa6d064e345a4ceff7cef4c6a1e71301
xx-isa <irmelo.filho@gmail.com>
2024-08-21 14:38:27 -0300

    adiciona keymap retomar fzf

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -11,6 +11,8 @@ return {
 
         opts.desc = "Show files in FZF"
         keymap.set("n", "<leader>F", fzf.files, opts)
+        opts.desc = "Resume FZF"
+        keymap.set("n", "<leader>rF", fzf.resume, opts)
         opts.desc = "Show references in FZF"
         keymap.set("n", "<leader>rr", fzf.lsp_references, opts)
     end,
