ab49ef0f91b9c32f72d6ad364b9cbb2706a53aba
xx-isa <irmelo.filho@gmail.com>
2024-09-23 15:56:36 -0300

    feat: adiciona spec do lualine

--- .config/nvim/lua/plugins/lightline.lua
+++ .config/nvim/lua/plugins/lightline.lua
@@ -1,45 +1,59 @@
-return {
-    "itchyny/lightline.vim",
-    dependencies = {
-        "tpope/vim-fugitive",
-    },
-    init = function()
-        local utils = require("utils")
-        vim.g.lightline = {
-            -- %L
-            colorscheme = utils.colorscheme,
-            active = {
-                left = {
-                    { "mode",     "paste" },
-                    { "gitbranch" },
-                    { "readonly", "filename" },
-                },
-                right = {
-                    { "lineinfo",    "linetotal" },
-                    { "percent" },
-                    { "fileencoding", "filetype" },
-                },
-            },
-            component = {
-                linetotal = "󰱴  %L",
-            },
-            component_function = {
-                filename = "LightlineFilename",
-                gitbranch = "FugitiveHead",
-                readonly = "LightlineReadonly",
-            },
+
+local lualine = {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {"nvim-tree/nvim-web-devicons", opt = true},
+    opts = {
+        options = {
+            component_separators = {left = '|', right = '|'},
+            section_separators = {left = '', right = ''},
         }
-
-        vim.cmd([[
-        function! LightlineReadonly()
-            return &readonly && &filetype !~# '\v(help)' ? 'RO' : ''
-        endfunction
-
-        function! LightlineFilename()
-            let filename = expand('%:t') !=# '' ? expand('%:t') : '[NONAME]'
-            let modified = &modified ? ' +' : ''
-            return filename . modified
-        endfunction
-        ]])
-    end,
+    }
 }
+
+-- local lightline = {
+--     "itchyny/lightline.vim",
+--     dependencies = {
+--         "tpope/vim-fugitive",
+--     },
+--     init = function()
+--         local utils = require("utils")
+--         vim.g.lightline = {
+--             -- %L
+--             colorscheme = utils.colorscheme,
+--             active = {
+--                 left = {
+--                     { "mode",     "paste" },
+--                     { "gitbranch" },
+--                     { "readonly", "filename" },
+--                 },
+--                 right = {
+--                     { "lineinfo",    "linetotal" },
+--                     { "percent" },
+--                     { "fileencoding", "filetype" },
+--                 },
+--             },
+--             component = {
+--                 linetotal = "󰱴  %L",
+--             },
+--             component_function = {
+--                 filename = "LightlineFilename",
+--                 gitbranch = "FugitiveHead",
+--                 readonly = "LightlineReadonly",
+--             },
+--         }
+--
+--         vim.cmd([[
+--         function! LightlineReadonly()
+--             return &readonly && &filetype !~# '\v(help)' ? 'RO' : ''
+--         endfunction
+--
+--         function! LightlineFilename()
+--             let filename = expand('%:t') !=# '' ? expand('%:t') : '[NONAME]'
+--             let modified = &modified ? ' +' : ''
+--             return filename . modified
+--         endfunction
+--         ]])
+--     end,
+-- }
+
+return lualine
