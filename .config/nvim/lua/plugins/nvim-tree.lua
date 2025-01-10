b992a438c76d5aa758b6aafc78b6d258fba53241
xx-isa <irmelo.filho@gmail.com>
2025-01-10 14:02:48 -0300

    (nvim) desativa nvim-tree

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,6 +1,9 @@
 return {
     "nvim-tree/nvim-tree.lua",
-    --cmd = {"NvimTreeOpen", "NvimTreeToggle"},
+    keys = {
+        { "<C-n>", "<Cmd>NvimTreeToggle<CR>", noremap = true, silent = true, desc = "Toggle NvimTree"}
+    },
+    cmd = {"NvimTreeOpen", "NvimTreeToggle"},
     init = function ()
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
@@ -12,14 +15,4 @@ return {
             signcolumn = "auto"
         },
     },
-    config = function (_, opts)
-        require("nvim-tree").setup(opts)
-
-        vim.keymap.set(
-            "n",
-            "<C-n>",
-            "<Cmd>NvimTreeToggle<CR>",
-            {noremap = true, silent = true, desc = "Toggle NvimTree"}
-        )
-    end
 }
